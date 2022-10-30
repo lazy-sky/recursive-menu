ad17ac3123449b392ced75096d2e1f3fbb2f3a6b
lazy-sky <1cielo@naver.com>
2022-10-30 19:50:03 +0900

    Feat: Make delete button in MenuItem

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { TreeNode } from '../api'
 import ArrowDownIcon from '../assets/icons/arrowDown.js'
 import ArrowRightIcon from '../assets/icons/arrowRight.js'
+import XIcon from '../assets/icons/x.js'
 
 import './menuItem.css'
 
@@ -13,24 +14,45 @@ interface IMenuItemProps {
 const MenuItem = ({ item }: IMenuItemProps) => {
   const { type, name, children = [] } = item
   const [isOpen, setIsOpen] = useState(false)
+  const [isDeleted, setIsDeleted] = useState(false)
 
   const handleArrowToggle = () => {
     setIsOpen(prev => !prev)
   }
 
+  const handleXClick = () => {
+    setIsDeleted(true)
+  }
+
+  if (isDeleted) return null
+
   if (type === 'folder') {
     return (
       <div className='menuItem'>
-        <button type='button' onClick={handleArrowToggle} className='toggleBtn'>
-          {isOpen ? <ArrowDownIcon /> : <ArrowRightIcon />}
+        <div className='folder'>
+          <button type='button' onClick={handleArrowToggle} >
+            {isOpen ? <ArrowDownIcon /> : <ArrowRightIcon />}
+          </button>
           {item.name}
-        </button>
+          <button type='button' onClick={handleXClick}>
+            <XIcon />
+          </button>
+        </div>
         {isOpen && children.map(child => <MenuItem key={child.id} item={child} />)}
       </div>
     )
   }
 
-  return <div className='menuItem'>{name}</div>
+  return (
+    <div className='menuItem'>
+      <div className='file'>
+        {name}
+        <button type='button' onClick={handleXClick}>
+          <XIcon />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default MenuItem
